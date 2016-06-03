bf92e270783240f5ab53f65fb7999ceb019de525
srajah.shakila@gmail.com <srajah.shakila@gmail.com>
2016-06-03 16:00:34 +0530

    Changing the version after releasing the tag

--- src/test/java/org/wso2/carbon/connector/integration/test/amazonsqs/AmazonsqsConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/amazonsqs/AmazonsqsConnectorIntegrationTest.java
@@ -63,7 +63,7 @@ public class AmazonsqsConnectorIntegrationTest extends ConnectorIntegrationTestB
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("amazonsqs-connector-1.0.1");
+        init("amazonsqs-connector-1.0.2-SNAPSHOT");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "");

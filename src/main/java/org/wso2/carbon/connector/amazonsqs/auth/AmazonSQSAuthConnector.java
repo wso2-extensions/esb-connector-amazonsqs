675856216e01c962900d5226644f3f50884df91a
Sajinie <sajiniekavindya@gmail.com>
2024-06-06 10:59:13 +0530

    Trim the input before checking whether it is a JSON payload

--- src/main/java/org/wso2/carbon/connector/amazonsqs/auth/AmazonSQSAuthConnector.java
+++ src/main/java/org/wso2/carbon/connector/amazonsqs/auth/AmazonSQSAuthConnector.java
@@ -107,7 +107,7 @@ public class AmazonSQSAuthConnector extends AbstractConnector {
                 payloadBuilder.append(URLEncoder.encode(entry.getKey(), charSet));
                 payloadBuilder.append(AmazonSQSConstants.EQUAL);
                 if (entry.getKey().equals(AmazonSQSConstants.API_MESSAGE_BODY)
-                        && AmazonSQSConstants.JSON_START_CHARACTER.contains(entry.getValue().substring(0, 1))) {
+                        && AmazonSQSConstants.JSON_START_CHARACTER.contains(entry.getValue().trim().substring(0, 1))) {
 
                     // Create a JSON string payload as same as the payload we attach to the message context in below
                     String jsonStr = getJsonString(entry);
@@ -139,7 +139,7 @@ public class AmazonSQSAuthConnector extends AbstractConnector {
                 payloadStrBuilder.append(AmazonSQSConstants.COLON);
                 payloadStrBuilder.append('"');
                 if (entry.getKey().equals(AmazonSQSConstants.API_MESSAGE_BODY)
-                        && AmazonSQSConstants.JSON_START_CHARACTER.contains(entry.getValue().substring(0, 1))) {
+                        && AmazonSQSConstants.JSON_START_CHARACTER.contains(entry.getValue().trim().substring(0, 1))) {
                     payloadStrBuilder.append(entry.getValue().replace(System.lineSeparator(), "").
                             replaceAll("(?<!\\\\)\\\\\"", "\\\\\\\\\\\\\"").replace("\"", "\\\"").
                             replace("\\\\\"", "\\\"").replaceAll("(?<!\\\\)\\\\n", "\\\\\\\\n"));
